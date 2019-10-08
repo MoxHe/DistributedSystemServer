f918861c7f20392be61f977e5ccbb88c06414c7c
MoxiaoHe <he.moxiao@gmail.com>
2019-10-07 21:34:53 -0700

    add draft

---
+++ src/main/java/SkierServlet.java
@@ -0,0 +1,78 @@
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class SkierServlet extends javax.servlet.http.HttpServlet {
+
+  protected void doPost(javax.servlet.http.HttpServletRequest request,
+      javax.servlet.http.HttpServletResponse response)
+      throws javax.servlet.ServletException, IOException {
+
+    PrintWriter out = response.getWriter();
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    String urlPath = request.getPathInfo();
+
+    if (urlPath == null || urlPath.length() == 0) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      String jsonString = new Gson().toJson("INVALID URL");
+      out.write(jsonString);
+      return;
+    }
+
+    String[] urlParts = urlPath.split("/");
+
+    if (!isUrlValid(urlParts)) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      String jsonString = new Gson().toJson("INVALID URL");
+      out.write(jsonString);
+    } else {
+      response.setStatus(HttpServletResponse.SC_OK);
+
+      BufferedReader buf = request.getReader();
+
+      out.write(new Gson().toJson(200));
+    }
+
+
+  }
+
+  protected void doGet(javax.servlet.http.HttpServletRequest request,
+      javax.servlet.http.HttpServletResponse response)
+      throws javax.servlet.ServletException, IOException {
+
+
+    PrintWriter out = response.getWriter();
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    String urlPath = request.getPathInfo();
+
+    if (urlPath == null || urlPath.length() == 0) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      String jsonString = new Gson().toJson("INVALID URL");
+      out.write(jsonString);
+      return;
+    }
+
+    String[] urlParts = urlPath.split("/");
+    // and now validate url path and return the response status code
+    // (and maybe also some value if input is valid)
+
+    if (!isUrlValid(urlParts)) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      String jsonString = new Gson().toJson("INVALID URL");
+      out.write(jsonString);
+    } else {
+      response.setStatus(HttpServletResponse.SC_OK);
+      String jsonString = new Gson().toJson(200);
+      out.write(jsonString);
+    }
+  }
+
+  private boolean isUrlValid(String[] urlPath) {
+    return true;
+  }
+}
